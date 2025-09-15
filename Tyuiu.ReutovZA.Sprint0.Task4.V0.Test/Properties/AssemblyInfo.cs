4d63850bfd36bf7ab79e0e766a45aa6f088e088a
ReutovZA <84906609+rerer-ReutovZA@users.noreply.github.com>
2025-09-15 15:47:11 +0500

    Task4

---
+++ Tyuiu.ReutovZA.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SpirinIS.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("Tyuiu.SpirinIS.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b8db3ee0-1eec-49b1-ac93-10958c163668")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

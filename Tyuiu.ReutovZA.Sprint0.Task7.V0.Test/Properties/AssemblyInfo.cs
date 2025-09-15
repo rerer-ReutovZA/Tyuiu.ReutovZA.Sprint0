aefe81706bb987ab5b921f78b34d25530e45f701
ReutovZA <84906609+rerer-ReutovZA@users.noreply.github.com>
2025-09-15 16:09:26 +0500

    Task7

---
+++ Tyuiu.ReutovZA.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SpirinIS.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("Tyuiu.SpirinIS.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d378207-647f-4d3f-a860-c12fd3817e0c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

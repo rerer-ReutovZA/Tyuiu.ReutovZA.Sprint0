4a282cc5e03d330beef77388cb3e5ba192f0b7b7
Захар Реутов <reutovz809@gmail.com>
2025-09-15 13:31:10 +0500

    Task2

---
+++ Tyuiu.ReutovZA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ReutovZA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.ReutovZA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Захар";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет,Захар", res);
+        }
+    }
+}
